68b028dd3dad31c7f72e6d7f3634bd5df3d2344e
Balázs <40501930+klmnblzs@users.noreply.github.com>
2024-10-10 19:46:16 +0200

    Revert "Merge pull request #3 from klmnblzs/backend"
    This reverts commit 90cfb0b4cf9dd0e748113fc054d354485f43fc16, reversing
    changes made to e89e9b6a44690b02f8b4b8646aadaf276c2d9e13.

---
+++ frontend/src/app/main/cars/car-about/car-about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './car-about.component.html',
+  styleUrl: './car-about.component.scss'
+})
+export class CarAboutComponent {
+
+}
---
+++ frontend/src/app/main/cars/car/car.model.ts
@@ -0,0 +1,8 @@
+export interface carData {
+    id: string,
+    image: string,
+    brand: string,
+    model: string,
+    type: string,
+    price_per_day: string
+}
