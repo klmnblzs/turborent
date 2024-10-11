2ab85576e21c86ee945bf8903a6687e7ba3b9ef2
adamasakal <pidl.adam05@gmail.com>
2024-10-11 13:24:17 +0200

    Added registration & login
    -Added login & register
    -mysql -> mysql2

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
+const cors=require('cors')
 
+const bcrypt=require('bcrypt')
 
 const app = express();
-dotenv.config();
 
 // FONTOS A BODY PARAMOKHOZ!
 // CORS POLICY AZ ANGULAR MIATT
-app.use(express.json(), (req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
-    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, id");
-  
-    next();
-});
+app.use(express.json())
+
+app.use(cors({
+    origin: '*', // Csak az Angular alkalmazás engedélyezése
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Engedélyezett HTTP-módszerek
+    allowedHeaders: ['Content-Type', 'id'], // Engedélyezett fejléc
+}));
+
+dotenv.config();
 
 const pool = mysql.createPool({
     connectionLimit: 10,
+    queueLimit: 0,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
@@ -34,39 +38,142 @@ app.get('/', (req, res) => {
     res.send('Welcome to the API');
 });
 
-app.get('/cars', (req, res) => {
-    const carId = req.headers['id']
+app.get('/cars', async (req, res) => {
+    const carId = req.headers['id'];
 
-    if(carId){
-        pool.query('call GetCarById(?)', [carId], (err, results) => {
-            if(err) return res.status(500).json({error: err.message});
+    try {
+        if (carId) {
+            const [results] = await pool.query('CALL GetCarById(?)', [carId]);
             res.json(results[0]);
-        })
-        return true
+        } else {
+            const [results] = await pool.query('CALL ListCars()');
+            res.json(results[0]);
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
     }
-
-    pool.query('call ListCars', (err, results) => {
-        if(err) return res.status(500).json({error: err.message});
-        res.json(results[0]);
-    })
-    
 });
 
-app.get('/filter/categories', (req, res) => {
-    pool.query('call GetAllCarCategories', (err, results) => {
-        if(err) return res.status(500).json({error: err.message});
-        res.json(results[0]);
-    })
+app.post('/customers/register', async (req, res) => {
+    const {
+        first_name,
+        last_name,
+        email,
+        phone_number,
+        driver_license_picture_front,
+        driver_license_picture_back,
+        date_of_birth,
+        post_code,
+        city,
+        street,
+        house_number,
+        password,
+        isApproved,
+        isAdmin
+      } = req.body;
+
+      if (!first_name) return res.status(400).json({ message: 'First name is required' });
+      if (!last_name) return res.status(400).json({ message: 'Last name is required' });
+      if (!email) return res.status(400).json({ message: 'Email is required' });
+      if (!phone_number) return res.status(400).json({ message: 'Phone number is required' });
+      if (!driver_license_picture_front) return res.status(400).json({ message: 'Driver license front picture is required' });
+      if (!driver_license_picture_back) return res.status(400).json({ message: 'Driver license back picture is required' });
+      if (!date_of_birth) return res.status(400).json({ message: 'Date of birth is required' });
+      if (!post_code) return res.status(400).json({ message: 'Post code is required' });
+      if (!city) return res.status(400).json({ message: 'City is required' });
+      if (!street) return res.status(400).json({ message: 'Street is required' });
+      if (!house_number) return res.status(400).json({ message: 'House number is required' });
+      if (!password) return res.status(400).json({ message: 'Password is required' });
+      if (isApproved === undefined) return res.status(400).json({ message: 'Approval status (isApproved) is required' });
+      if (isAdmin === undefined) return res.status(400).json({ message: 'Admin status (isAdmin) is required' });    
+
+    try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        try {
+            const [result] = await pool.execute('CALL RegisterCustomers(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+                first_name,
+                last_name,
+                email,
+                phone_number,
+                driver_license_picture_front,
+                driver_license_picture_back,
+                date_of_birth,
+                post_code,
+                city,
+                street,
+                house_number,
+                hashedPassword,
+                isApproved,
+                isAdmin
+              ])
+
+            if (result.affectedRows > 0) {
+                res.status(201).json({ message: 'User registered successfully' });
+            } else {
+                res.status(500).json({ message: 'Failed to register user' });
+            }
+        } catch (err) {
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+app.post('/customers/login', async (req,res) => {
+    const {
+        email,
+        password
+    } = req.body
+
+    if (!email) return res.status(400).json({ message: 'Email is required' });
+    if (!password) return res.status(400).json({ message: 'Password is required' });
+
+    try {
+        const [hashedPassword] = await pool.query("SELECT password FROM Customers WHERE email = ?", [email])
+        const validPassword = hashedPassword[0].password.trim()
+        const isPasswordValid = await bcrypt.compare(password, validPassword)
+
+        if(isPasswordValid) {
+            try {
+                const [results] = await pool.query("CALL LoginCustomer(?, ?)", [ email, validPassword ])
+                res.send(results[0])
+            } catch (err) {
+                res.status(500).json({ error: "Internal server error" })
+            }
+        } else {
+            res.status(401).json({ error: "Wrong password" })
+        }
+    } catch(err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+app.post('/customers/login'), async (req,res) => {
+    const {
+        email,
+        password
+    } = req.body
     
+    if (!email) return res.status(400).json({ message: 'Email is required' });
+    if (!password) return res.status(400).json({ message: 'Password is required' });
+
+    try {
+        const [hashedPassword] = await pool.query("SELECT password FROM Customers WHERE email = ?", [email])
+    } catch(err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+app.get('/filter/categories', async (req, res) => {
+    const [results] = await pool.query("CALL GetAllCarCategories")
+    res.json(results[0])
 });
 
-app.get('/filter/brands', (req, res) => {
-    pool.query('call GetAllCarBrands', (err, results) => {
-        if(err) return res.status(500).json({error: err.message});
-        res.json(results[0]);
-    })
-    
+app.get('/filter/brands', async (req, res) => {
+    const [results] = await pool.query("CALL GetAllCarBrands")
+    res.json(results[0])
 });
-
-
-
