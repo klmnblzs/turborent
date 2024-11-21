51bd95cafa3978b0cdceda541c157a7446e284c0
adamasakal <pidl.adam05@gmail.com>
2024-11-21 17:30:30 +0100

    Registration approval
    Registration approval implemented

--- backend/app.js
+++ backend/app.js
@@ -61,7 +61,7 @@ function authenticateToken(req, res, next) {
 }
 
 function generateAccessToken(data) {
-    return jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' })
+    return jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
 }
 
 async function generateRefreshToken(userId) {
@@ -221,11 +221,14 @@ app.post('/auth/register', upload.fields([ { name:'licensePictureFront' }, { nam
                     'INSERT INTO driver_license_pictures (customer_id, picture_front, picture_back) VALUES (?, ?, ?)',
                     [customerId, frontImage, backImage]
                 );
+                const pictureId = pictures.insertId
 
-                if (pictures.affectedRows > 0) {
-                    return res.status(201).json({ message: 'User and license pictures registered successfully' });
+                const [approval] = await pool.execute('CALL CreateApproval(?,?)', [customerId, pictureId] )
+
+                if (pictures.affectedRows > 0 && approval.affectedRows > 0) {
+                    return res.status(201).json({ message: 'User, approval and license pictures registered successfully' });
                 } else {
-                    return res.status(500).json({ message: 'Failed to insert license pictures' });
+                    return res.status(500).json({ message: 'Failed to insert user, approval and license pictures' });
                 }
             } else {
                 console.log('No rows affected, customer not created:', customers);
@@ -238,6 +241,222 @@ app.post('/auth/register', upload.fields([ { name:'licensePictureFront' }, { nam
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: 'Internal server error (második catch)' });
+    }
+})
+
+app.post('/auth/refresh', async (req, res) => {
+    const { refreshToken } = req.body;
+
+    if (!refreshToken) {
+        return res.status(400).json({ message: "Adj meg egy refresh tokent" });
+    }
+
+    try {
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, user) => {
+            if (err) return res.status(403).json({ message: "Érvénytelen refresh token" });
+
+            const [rows] = await pool.execute('SELECT * FROM refresh_tokens WHERE token = ?', [refreshToken]);
+
+            if (rows.length === 0) {
+                return res.status(403).json({ message: "Érvénytelen refresh token" });
+            }
+
+            await pool.execute('DELETE FROM refresh_tokens WHERE token = ?', [refreshToken]);
+
+            const newRefreshToken = await generateRefreshToken(user.userId);
+            const newAccessToken = generateAccessToken({ id: user.userId }); 
+
+            res.json({ accessToken: newAccessToken, refreshToken: newRefreshToken });
+        });
+    } catch (err) {
+        if (err.name === "JsonWebTokenError") {
+            return res.status(403).json({ message: "Érvénytelen refresh token" });
+        }
+        console.error(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+app.post('/admin/car/delete', authenticateToken, async (req, res) => {
+    const {
+        id
+    } = req.body
+
+    if(!id) return res.status(400).json({ message: "Car ID is required" })
+    
+    try {
+        const [results] = await pool.execute("CALL DeleteCar(?)", [ id ])
+
+        if(results) {
+            res.status(200).json({ message: "Car deleted!" })
+        } else {
+            res.status(500).json({ error: "Error while deleting car" })
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
+
+app.post('/admin/approvals/approve', async (req, res) => {
+    const { customer_id, admin_id } = req.body;
+
+    try {
+        const [results] = await pool.query('CALL ApproveRequest(?, ?)', [ customer_id, admin_id ]);
+
+        if(results.affectedRows > 0) {
+            return res.status(200).json({message:"Request Approved"})
+        } else {
+            return res.status(400).json({message:"Approve failed"})
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+app.post('/admin/approvals/deny', async (req, res) => {
+    const { customer_id } = req.body;
+
+    try {
+        const [results] = await pool.query('CALL DenyApproveRequest(?)', [ customer_id ]);
+
+        if(results.affectedRows > 0) {
+            return res.status(200).json({message:"Request Approved"})
+        } else {
+            return res.status(400).json({message:"Approve failed"})
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+app.post('/admin/car/add', authenticateToken, upload.fields([ { name: 'thumbnail' } ]), async (req, res) => {
+    const {
+        brand,
+        model,
+        cc,
+        year,
+        licensePlate,
+        category,
+        available,
+        pricePerDay,
+        mileage,
+        isDiesel,
+        lastServiceDate,
+        seats,
+        doors,
+        isManual,
+        description,
+        equipments,
+      } = req.body;
+      
+    if (!brand) return res.status(400).json({ message: 'Brand is required' });
+    if (!model) return res.status(400).json({ message: 'Model is required' });
+    if (!cc) return res.status(400).json({ message: 'Engine capacity (cc) is required' });
+    if (!year) return res.status(400).json({ message: 'Year is required' });
+    if (!licensePlate) return res.status(400).json({ message: 'License plate is required' });
+    if (!category) return res.status(400).json({ message: 'Category is required' });
+    if (!available) return res.status(400).json({ message: 'Available is required' });
+    if (!pricePerDay) return res.status(400).json({ message: 'Price per day is required' });
+    if (!mileage) return res.status(400).json({ message: 'Mileage is required' });
+    if (!seats) return res.status(400).json({ message: 'Seats is required' });
+    if (!doors) return res.status(400).json({ message: 'Doors is required' });
+    if (!isDiesel) return res.status(400).json({ message: 'Fuel type (isDiesel) is required' });
+    if (!isManual) return res.status(400).json({ message: 'Transmission type (isManual) is required' });
+    if (!lastServiceDate) return res.status(400).json({ message: 'Last service type is required' });
+    if (!description) return res.status(400).json({ message: 'Description is required' });
+    if (!equipments) return res.status(400).json({ message: 'Equipments are required!' });
+
+    const thumbnailImage = req.files['thumbnail'][0].buffer;
+    if (!thumbnailImage) return res.status(400).json({ message: 'Thumbnail' });
+
+    try {
+        const [pictures] = await pool.execute(
+            'INSERT INTO car_pictures (thumbnail) VALUES (?)',
+            [thumbnailImage]
+        );
+
+        if(pictures.affectedRows > 0) {
+            const pictureId = pictures.insertId
+
+            const [results] = await pool.execute('CALL AddCar2(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', 
+                [
+                    brand,
+                    model,
+                    cc,
+                    year,
+                    licensePlate,
+                    category,
+                    available,
+                    pricePerDay,
+                    mileage,
+                    isDiesel,
+                    lastServiceDate,
+                    seats,
+                    doors,
+                    isManual,
+                    pictureId
+                ])
+            
+            if(results.length > 0 && results[0].length > 0) {
+                const carId = results[0][0]['LAST_INSERT_ID()']
+
+                const [insertDescription] = await pool.execute(
+                    'INSERT INTO car_data (car_id, description, equipments) VALUES (?,?,?)',
+                    [carId, description, equipments]
+                );
+
+                if(insertDescription.affectedRows > 0) {
+                    res.status(200).json({ message: "Success!" })
+                } else {
+                    res.status(500).json({ message: "Error!" })
+                }
+            } else {
+                res.status(500).json({ message: "Error!" })
+            }
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+app.get('/admin/approvals', authenticateToken, async (req, res) => {
+    const [results] = await pool.query('CALL ListApprovals()');
+
+    res.json(results[0]);
+})
+
+app.get('/admin/approvals/:id', authenticateToken, async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const [results] = await pool.query('CALL GetApprovalById(?)', [ id ]);
+
+        if(results.length > 0) {
+            for (let approval of results[0]) {
+                const [imageResults] = await pool.query('SELECT picture_front, picture_back FROM driver_license_pictures WHERE id = ?', [approval.picture_set_id]);
+
+                if (imageResults.length > 0) {
+                    const frontImageBase64 = Buffer.from(imageResults[0].picture_front).toString('base64');
+                    approval.picture_front = `data:image/jpeg;base64,${frontImageBase64}`;
+
+                    const backImageBase64 = Buffer.from(imageResults[0].picture_back).toString('base64');
+                    approval.picture_back = `data:image/jpeg;base64,${backImageBase64}`;
+                } else {
+                    approval.picture_front = null;
+                    approval.picture_back = null;
+                }
+            }
+            res.json(results[0]);
+        } else {
+            res.status(400).json({ message: "Error while fetching approval" });
+        }
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message: "Internal server error" });
     }
 })
 
@@ -258,7 +477,7 @@ app.post('/auth/check-duplicate', async (req, res) => {
         return res.status(200).json({ message: "Email is available" });
 
     } catch (err) {
-        console.error("Error checking email:", err);
+        console.log("Error checking email:", err);
         res.status(500).json({ message: "Internal server error" });
     }
 });
@@ -279,13 +498,25 @@ app.post('/auth/login', async (req,res) => {
             if(results[0].length == 0) {
                 res.status(401).json({ error: "Wrong password"})
             } else {
-                const user = results[0][0]; 
+                const user = results[0][0];
+                
                 req.body.id=user.id
                 req.body.firstname=user.first_name
                 req.body.lastname=user.last_name
+                req.body.email=user.email
+                req.body.phonenumber=user.phone_number
+                req.body.dateofbirth=user.date_of_birth
+                req.body.postcode=user.post_code
+                req.body.city=user.city
+                req.body.street=user.street
+                req.body.housenumber=user.house_number
+                req.body.password=user.password
+                req.body.isApproved=user.isApproved
+                req.body.isAdmin=user.isAdmin
+
                 const accessToken = generateAccessToken(req.body)
                 const refreshToken = await generateRefreshToken(user.id)
-                res.status(200).json({ token: accessToken, refreshToken: refreshToken })
+                res.status(200).json({ token: accessToken, refreshToken: refreshToken, userid: user.id })
             }
         } else {
             res.status(401).json({ error: "Wrong password" })
@@ -298,10 +529,8 @@ app.post('/auth/login', async (req,res) => {
 
 app.post('/auth/logout', async (req, res) => {
     const { refreshToken } = req.body;
-    console.log("LEFUTOTT")
 
     if (!refreshToken) {
-        console.log("refresh required")
         return res.status(400).json({ message: "Refresh Token is required" });
     }
 
@@ -309,14 +538,12 @@ app.post('/auth/logout', async (req, res) => {
         const [rows] = await pool.execute('SELECT * FROM refresh_tokens WHERE token = ?', [refreshToken.replace(/"/g, "")]);
   
         if (rows.length === 0) {
-            console.log("rows")
             return res.status(404).json({ message: "Refresh token was not found in the database" });
         }
 
         const [result] = await pool.execute('DELETE FROM refresh_tokens WHERE token = ?', [refreshToken.replace(/"/g, "")]);
 
         if (result.affectedRows > 0) {
-            console.log("result")
             return res.status(200).json({ message: "Logged out!" });
         } else {
             return res.status(500).json({ message: "Token couldn't be deleted" });
@@ -327,6 +554,89 @@ app.post('/auth/logout', async (req, res) => {
         return res.status(500).json({ message: "Error while logging out." });
     }
 });
+
+app.get('/user/data/:id', async (req, res) => {
+    const userid = req.params.id
+
+    try {
+        const [results] = await pool.execute('CALL GetCustomerById(?)', [ userid ])
+        res.send(results[0][0])
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: "Internal server error" })
+    }
+})
+
+app.post('/user/edit/', authenticateToken, async (req, res) => {
+    const {
+        firstname,
+        lastname,
+        postcode,
+        city,
+        street,
+        housenum,
+        id
+    } = req.body
+
+    try {
+        const [results] = await pool.execute('CALL UpdateCustomerPersonalData(?,?,?,?,?,?,?)', [ firstname, lastname, postcode, city, street, housenum, id ])
+    
+        if(results) {
+            return res.status(200).json({ message: "Customer data updated" })
+        } else {
+            return res.status(500).json({ message: "Error while updating customer's personal data" });
+
+        }
+    } catch(err) {
+        console.log(err)
+        return res.status(500).json({error: "Internal server error"})
+    }
+
+
+})
+
+app.post('/user/edit/password', authenticateToken, async (req, res) => {
+    const {
+        id,
+        oldpassword,
+        newpassword
+    } = req.body
+
+    
+    try {
+        [hashedPassword] = await pool.query("SELECT password FROM Customers WHERE id = ?", [id])
+        const oldPassword = hashedPassword[0].password.trim()
+        const isPasswordValid = await bcrypt.compare(oldpassword, oldPassword)
+
+        try {
+            if(isPasswordValid) { 
+                const saltRounds = 10;
+                const newHashedPassword = await bcrypt.hash(newpassword, saltRounds);
+
+                const [results] = await pool.execute('CALL UpdateCustomerPassword(?,?,?)', [ id, oldPassword, newHashedPassword ])
+                console.log(results)
+
+                if (results.affectedRows > 0) {
+                    res.status(200).json({ message: "Password updated!" })
+                } else {
+                    res.status(401).json({message: "Error while updating password"})
+                }
+                 
+            } else {
+                return res.status(401).json({error: "Password invalid"})
+            }
+            
+
+        } catch(err) {
+            console.log(err)
+            return res.status(500).json({error: "MySQL server error"})
+        }
+        
+    } catch(err) {
+        console.log(err)
+        return res.status(500).json({error: "Internal server error"})
+    }
+})
 
 app.get('/filter/categories', async (req, res) => {
     const [results] = await pool.query("CALL GetAllCarCategories")
